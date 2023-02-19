1d13b89250e165d3e9e2895cfc0c734de7ecba97
Masterjonathancode <jon.a.mammen@gmail.com>
2023-02-19 00:59:41 +0000

    Completed Hello World Lab

--- src/main/java/Expressions.java
+++ src/main/java/Expressions.java
@@ -2,8 +2,13 @@ public class Expressions
 {
     boolean A = true; 
     boolean B = false;  
+    public static void main(String[] args){
+        System.out.println("Hello World");
+        System.out.println("My name is Jonathan");
+      }
 }
 // Boolean class assignments denoted as either true or false.// 
+// The last set of lines of code are now copied from the "Hello World Program."//
 class Bool1{ 
     public static void main(String args[]){
  
@@ -25,7 +30,8 @@ class Bool1{
        System.out.println("(A|B)&A = "+((A|B)&A));
        else
        System.out.println("!=B = "+(A));
-      
-     }
+    
+       }
+    
  }
  //This program prints out the Java boolean logical operators with logical conditions of if,else if, and else statements.// 
